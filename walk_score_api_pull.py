9357a36e755a9554dbf6ab59eaced2ba278376ac
Brian Bales <bbales11@gmail.com>
2017-12-20 13:57:43 -0500

    full update of may branch

--- walk_score_api_pull.py
+++ walk_score_api_pull.py
@@ -18,29 +18,38 @@
 # Dependencies
 import json
 import requests as req
+import pandas as pd
 
+#import VA lat long file
+df = pd.read_csv('Random VA lat longs.csv')
+#print(df.head())
 # Save config information.
-example_string = 'http://api.walkscore.com/score?format=json& \
-address=1119%8th%20Avenue%20Seattle%20WA%2098101&lat=47.6085& \
-lon=-122.3295&transit=1&bike=1&wsapikey=4e9a14b4cda5f4c809d23bf0f13235d4'
+# example_string = 'http://api.walkscore.com/score?format=json& \
+# address=1119%8th%20Avenue%20Seattle%20WA%2098101&lat=47.6085& \
+# lon=-122.3295&transit=1&bike=1&wsapikey=4e9a14b4cda5f4c809d23bf0f13235d4'
 walkscore_url = 'http://api.walkscore.com/score?'
 formatter = 'json'
-api_key = '4e9a14b4cda5f4c809d23bf0f13235d4'
+api_key = 'c58332edc3aafbea28ad89d47614ff8c'
 address = '22031'
-lat = 38.860865
-lon = -77.280209
 
-param_dict = {
+
+# Get weather data
+for index, row in df.iterrows():
+	x = row['lat'],
+	y = row['lon'],
+	param_dict = {
 	'wsapikey': api_key,
-	'address': address,
+	#'address': address,
 	'format': formatter,
 	'transit': 1,
 	'bike': 1,
-	'lat': lat,
-	'lon': lon
-}
+	'lat': x,
+	'lon': y
+	}
+	ws_response = req.get(walkscore_url, params=param_dict)
+	
+	df.set_value(index,'walk score',ws_response["walkscore"])
+	df.set_value(index,'transit score',ws_response["transit"])
+	df.set_value(index,'bike score',ws_response["bike"])
+	print(row)
 
-# Get weather data
-ws_response = req.get(walkscore_url, params=param_dict)
-ws_json = ws_response.json()
-print(ws_json)
